7ff2c16ef5e52faaadad1447391ec0894422ab8b
ShammyKruz <orjisamuelama@gmail.com>
2023-10-12 22:05:57 +0100

    Product Listing

---
+++ frontend/src/data.ts
@@ -0,0 +1,77 @@
+import { Product } from "./types/Product";
+import App from './App';
+
+export const sampleProducts:Product[] = [
+    {
+        name: 'Treadmill Machine',
+        slug: 'Treadmill Machine',
+        category: 'Fitness',
+        image: '../images/treadmill.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'MMP',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'High quality treadmill invention'
+    },
+    {
+        name: 'Adjustable Dumbells',
+        slug: 'Adjustable Dumbells',
+        category: 'Gym',
+        image: '../images/treadmill.jpg',
+        price: 120,
+        countInStock: 20,
+        brand: 'MMP',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'High quality dumbell inventions'
+    },
+    {
+        name: 'Treadmill Machine',
+        slug: 'Treadmill Machine',
+        category: 'Fitness',
+        image: '../images/treadmill.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'MMP',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'High quality treadmill invention'
+    },
+    {
+        name: 'Treadmill Machine',
+        slug: 'Treadmill Machine',
+        category: 'Fitness',
+        image: '../images/treadmill.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'MMP',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'High quality treadmill invention'
+    },
+    {
+        name: 'Treadmill Machine',
+        slug: 'Treadmill Machine',
+        category: 'Fitness',
+        image: '../images/treadmill.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'MMP',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'High quality treadmill invention'
+    },
+    {
+        name: 'Treadmill Machine',
+        slug: 'Treadmill Machine',
+        category: 'Fitness',
+        image: '../images/treadmill.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'MMP',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'High quality treadmill invention'
+    },
+]
